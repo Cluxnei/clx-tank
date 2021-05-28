7c0e1d2fea939c48d299d296a502fa941de4e780
= <=>
2021-05-28 11:42:52 -0300

    find the shot point origin & fix sprite texture center

--- clx-tank/clx-tank/Player.h
+++ clx-tank/clx-tank/Player.h
@@ -12,10 +12,12 @@ class Player
 		sf::Vector2f velocity;
 		sf::Sprite shape;
 		Controls controls;
+		sf::Vector2f shootStartPoint;
 	public:
 		Player() {
 			this->position = sf::Vector2f(0.f, 0.f);
 			this->velocity = sf::Vector2f(1.f, 0.f);
+			this->shootStartPoint = this->shape.getTransform().transformPoint(32, 16);
 			this->shape = sf::Sprite();
 			this->shape.setPosition(this->position);
 		}
@@ -59,9 +61,26 @@ void Player::update(sf::RenderWindow *window) {
 	float angle = (atan2(dy, dx) + M_PI / 2.f) * 180.f / M_PI;
 	Utils::setOriginAndReadjust(this->shape, sf::Vector2f(diffOrigin, diffOrigin));
 	this->shape.setRotation(angle);
+	this->shootStartPoint = this->shape.getTransform().transformPoint(32, 16);
 }
 
 void Player::render(sf::RenderWindow *window) {
-	this->shape.setPosition(this->position);	
+	// sprite
+	this->shape.setPosition(this->position);
+	// shape
+	sf::RectangleShape r;
+	sf::FloatRect s = this->shape.getLocalBounds();
+	r.setSize(sf::Vector2f(s.width, s.height));
+	r.setFillColor(sf::Color::Green);
+	r.setPosition(sf::Vector2f(this->position.x, this->position.y));
+	Utils::setOriginAndReadjust(r, sf::Vector2f(8.f, 8.f));
+	r.setRotation(this->shape.getRotation());
+	window->draw(r);
+	// sprite
 	window->draw(this->shape);
+	// shot origin
+	sf::CircleShape c(3.f);
+	c.setFillColor(sf::Color::Red);
+	c.setPosition(this->shootStartPoint);
+	window->draw(c);	
 }
